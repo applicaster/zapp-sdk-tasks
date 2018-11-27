1e454fa3315ca626a90c3843782f6200a158e4f0
Ran Meirman <r.meirman@applicaster.com>
2018-11-27 13:15:10 -0500

    add easy way to include in Ruby projects

--- lib/zapp_sdk_tasks.rb
+++ lib/zapp_sdk_tasks.rb
@@ -3,7 +3,18 @@
 require "zapp_sdk_tasks/version"
 
 module ZappSdkTasks
+  class ZappRakeTasks
+    include Rake::DSL if defined? Rake::DSL
+
+    def install_tasks
+      path = File.expand_path(__dir__)
+      $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "lib"))
+      Dir.glob("#{path}/lib/**/*.rake").each { |f| load f }
+    end
+  end
 end
+
+ZappSdkTasks::ZappRakeTasks.new.install_tasks
 
 if defined?(Rails)
   module ZappSdkTasks
