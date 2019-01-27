16d12761ab135e6dc6a3f84ed04e68379591bf15
giltab <gil.tabak@gmail.com>
2019-01-27 10:31:26 +0200

    feat(upload dev project): Adding a task to upload a development project  (#9)

--- lib/zapp_sdk_tasks/s3_helper.rb
+++ lib/zapp_sdk_tasks/s3_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "uri"
+
 class S3Helper
   def self.resource_from_env_vars
     Aws::S3::Resource.new(
@@ -11,5 +13,12 @@ class S3Helper
 
   def self.write_options
     { acl: "public-read", cache_control: "max-age=60" }
+  end
+
+  def self.extract_cdn_uri(uri)
+    cdn_uri = URI(uri)
+    cdn_uri.host = nil
+    cdn_uri.path = "/" + cdn_uri.path
+    cdn_uri.to_s
   end
 end
